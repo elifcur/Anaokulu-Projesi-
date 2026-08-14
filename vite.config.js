c0104ec40a602bb549e79734245e5c6382c0410e
elifcur <elif.cur@tasarloji.com>
2026-08-14 14:31:56 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,6 @@ import react from '@vitejs/plugin-react'
 //
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.BASE_PATH || '/',
+  base: '/Anaokulu-Projesi/',
   plugins: [react()],
 })
